b3db464271828ad32637b46a3de6edd5cdafef66
Renato Costa <renato.rezende.costa@gmail.com>
2018-02-01 16:17:55 +0100

    T2.1 - Implemented authGuard

--- apps/flight-app/src/app/shared/auth/auth.guard.ts
+++ apps/flight-app/src/app/shared/auth/auth.guard.ts
@@ -9,7 +9,11 @@ export class AuthGuard implements CanActivate {
   constructor(private router:Router, private authService:AuthService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return undefined;
+    if(this.authService.userName){
+      return true;
+    }
+    this.router.navigate(['/home', {needsLogin: true}]);
+    return true;
   }
 
 }
