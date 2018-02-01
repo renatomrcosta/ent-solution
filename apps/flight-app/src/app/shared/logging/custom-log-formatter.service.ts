11bc8fa77edbcc39c0a3784cf9e94f5e9edeae95
Renato Costa <renato.rezende.costa@gmail.com>
2018-02-01 15:44:22 +0100

    T1.2 - Adjusted code

--- apps/flight-app/src/app/shared/logging/custom-log-formatter.service.ts
+++ apps/flight-app/src/app/shared/logging/custom-log-formatter.service.ts
@@ -1,6 +1,11 @@
+import {Injectable} from "@angular/core";
 import {LogFormatterService} from "logger-lib";
 
+@Injectable()
 export class CustomLogFormatterService implements LogFormatterService{
+
+  constructor() { }
+
   format(message: string): string {
     const now = new Date().toISOString();
     return `[${now}] ${message}`;
