014f5eab46e7ca1743e0a48f89ab728c794ac39d
Renato Costa <renato.rezende.costa@gmail.com>
2018-02-01 14:23:13 +0100

    T1S5 - Templated airport component ts file importing the airport service from the barrel

---
+++ apps/flight-app/src/app/flight-booking/airport/airport.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {AirportService} from "@flight-workspace/flight-api";
+
+@Component({
+  selector: 'airport',
+  templateUrl: './airport.component.html',
+  styleUrls: ['./airport.component.css']
+})
+export class AirportComponent implements OnInit {
+
+  get airports() {
+    return this.airportService.findAll();
+  }
+
+  constructor(
+    private airportService: AirportService) {
+  }
+
+  ngOnInit() {
+
+  }
+}
