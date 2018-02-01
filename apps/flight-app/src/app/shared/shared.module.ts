657dad01bc82f4078a9a339ff15a5a7c80957e4f
Renato Costa <renato.rezende.costa@gmail.com>
2018-02-01 15:28:10 +0100

    T1.2 - Added provider to shared module

--- apps/flight-app/src/app/shared/shared.module.ts
+++ apps/flight-app/src/app/shared/shared.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CityPipe } from './pipes/city.pipe';
 import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
+import {LogFormatterService} from "logger-lib";
+import {CustomLogFormatterService} from "./logging/custom-log-formatter.service";
 
 @NgModule({
   imports: [
@@ -19,6 +21,10 @@ export class SharedModule {
     return {
       ngModule: SharedModule,
       providers: [
+        {
+          provide: LogFormatterService,
+          useClass: CustomLogFormatterService
+        }
       ]
     }
   }
