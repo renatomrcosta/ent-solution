90b423dbdb9ebb3e41b15dc93e1e73a9780984d4
Renato Costa <renato.rezende.costa@gmail.com>
2018-02-01 16:07:00 +0100

    T2.1 - Imported router and activatedRouter in authGuard

--- apps/flight-app/src/app/shared/auth/auth.guard.ts
+++ apps/flight-app/src/app/shared/auth/auth.guard.ts
@@ -1,9 +1,13 @@
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from "@angular/router";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
 import {Observable} from "rxjs/Observable";
 import {Injectable} from "@angular/core";
+import {AuthService} from "./auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+
+  constructor(private router:Router, private authService:AuthService) {}
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     return undefined;
   }
