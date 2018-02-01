ebf1e004587cd2c930c23c4f95ffecbcdeb81b5a
Renato Costa <renato.rezende.costa@gmail.com>
2018-02-01 16:05:42 +0100

    T2.1 - Fixed injectable

--- apps/flight-app/src/app/shared/auth/auth.guard.ts
+++ apps/flight-app/src/app/shared/auth/auth.guard.ts
@@ -1,6 +1,8 @@
 import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from "@angular/router";
 import {Observable} from "rxjs/Observable";
+import {Injectable} from "@angular/core";
 
+@Injectable()
 export class AuthGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     return undefined;
