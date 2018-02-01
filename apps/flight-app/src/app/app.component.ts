aceefb4569680c0a8da2830d8a1e7804a9cd44c6
Renato Costa <renato.rezende.costa@gmail.com>
2018-02-01 15:20:26 +0100

    T1.1S9 - Reimported the service

--- apps/flight-app/src/app/app.component.ts
+++ apps/flight-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {LoggerService} from "logger-lib/logger.service";
+import {LoggerService} from "logger-lib";
 
 
 @Component({
@@ -9,8 +9,9 @@ import {LoggerService} from "logger-lib/logger.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
-  constructor(private router: Router, private log: LoggerService) {
-    log.log('BANANINHA!');
+  constructor(private router: Router, private loggerService: LoggerService) {
+    this.loggerService.log('ASD');
+    this.loggerService.debug('ASD');
   }
 }
 
