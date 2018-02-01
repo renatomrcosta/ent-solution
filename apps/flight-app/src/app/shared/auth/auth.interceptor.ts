882a12aaa7548c8295a394e47784bbe30b8ae1a8
Renato Costa <renato.rezende.costa@gmail.com>
2018-02-01 16:57:09 +0100

    T2.2 - Implemented interface

--- apps/flight-app/src/app/shared/auth/auth.interceptor.ts
+++ apps/flight-app/src/app/shared/auth/auth.interceptor.ts
@@ -1,6 +1,11 @@
 import {Injectable} from "@angular/core";
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
-export class AuthInterceptor {
+export class AuthInterceptor implements HttpInterceptor{
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return undefined;
+  }
 
 }
