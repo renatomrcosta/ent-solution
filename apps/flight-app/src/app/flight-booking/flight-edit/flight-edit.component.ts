81ecef834f195fa7232deab16d201b3cd269eab6
Renato Costa <renato.rezende.costa@gmail.com>
2018-02-01 16:49:39 +0100

    T2.2 - Added observable messaging

--- apps/flight-app/src/app/flight-booking/flight-edit/flight-edit.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -2,14 +2,18 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
+import {CanDeactivateComponent} from "../../shared/deactivation/can-deactivate-component";
 
 @Component({
   selector: 'app-flight-edit',
   templateUrl: './flight-edit.component.html'
 })
-export class FlightEditComponent implements OnInit {
+export class FlightEditComponent implements OnInit, CanDeactivateComponent {
+
   id: string;
   showDetails: string;
+  showWarning: boolean = false;
+  sender: Observer<boolean>;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -17,6 +21,19 @@ export class FlightEditComponent implements OnInit {
     this.route.params.subscribe(p => {
       this.id = p['id'];
       this.showDetails = p['showDetails'];
+    });
+  }
+
+  decide(decision:boolean):void{
+    this.showWarning = false;
+    this.sender.next(decision);
+    this.sender.complete();
+  }
+
+  canDeactivate(): Observable<boolean> {
+    return Observable.create((sender: Observer<boolean>)=>{
+      this.sender = sender;
+      this.showWarning = true;
     });
   }
 
